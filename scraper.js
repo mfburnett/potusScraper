20fcab0dab1eeeb3c2d709afde18aab0fefabd8e
Mackenzie Burnett <mfburnett@users.noreply.github.com>
2014-12-15 14:04:44 -0500

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -11,25 +11,25 @@ request('http://www.whitehouse.gov/briefing-room/speeches-and-remarks', function
 		var $ = cheerio.load(html);
 		
 		var date = $('#content').find('.date-line').each(function(i, v) {
-			listofDates.push(v.children[0].data);
+		  listofDates.push(v.children[0].data);
 		});
 
 		$('h3').children().each(function(index, value) {
-			var url = ('http://www.whitehouse.gov' + $(this).attr('href'));
-			request(url, function(e, r, html) {
-				var $ = cheerio.load(html);
+		  var url = ('http://www.whitehouse.gov' + $(this).attr('href'));
+	  	  request(url, function(e, r, html) {
+		    var $ = cheerio.load(html);
 				
-				var occurences = $('#content')
-          .find('p')
-          .text()
-          .toLowerCase()
-          .match(/ebola/g) || [];
+		    var occurences = $('#content')
+          	       .find('p')
+          	       .text()
+          	       .toLowerCase()
+          	       .match(/ebola/g) || [];
           
-				console.log('Came from: ' + url + '\n' +listofDates[index] + '\n' +occurences.length);
-			});
+		    console.log('Came from: ' + url + '\n' +listofDates[index] + '\n' +occurences.length);
 		});
+       	     });
 
-	}
+    }
 });
 
 //Find logical way to get all links in a single category 
